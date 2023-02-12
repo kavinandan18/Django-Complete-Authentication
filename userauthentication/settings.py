b546591b3af727a78522604d640af51d79362c7e
Avinandan Kumar <kavinandan18@gmail.com>
2023-02-12 12:50:30 +0530

    Initial commit

--- userauthentication/settings.py
+++ userauthentication/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +23,10 @@ STATIC_DIRS=BASE_DIR/ 'static'
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&vu9(5fz(up5v60)@tknaez*6qdls(emwpr6_ov=2sl8%r2oc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
