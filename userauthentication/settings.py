ad80ae05f74bd4a6a73d13d32da1476a8c495c80
Avinandan Kumar <kavinandan18@gmail.com>
2023-02-12 13:05:48 +0530

    first commit

--- userauthentication/settings.py
+++ userauthentication/settings.py
@@ -26,7 +26,7 @@ STATIC_DIRS=BASE_DIR/ 'static'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
